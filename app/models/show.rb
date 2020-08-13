eb3fc341272ed2d6b8d1de20a1b9caf1d4304231
Jenise Ruehle <jela07@gmail.com>
2020-08-13 15:50:09 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,8 @@ class Show < ActiveRecord::Base
   end 
   
   def self.most_popular_show
-    self.maximum(:rating).order(rating: :asc).first
+    mps = self.maximum(:rating)
+    mps.order(rating: :asc)
   end 
   
 end 
